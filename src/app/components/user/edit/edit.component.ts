797874ef161b6e61dc06468a2eeef90506c08d51
Arlie Rivas <arliedamir@gmail.com>
2024-08-25 12:24:48 -0500

    Create Add and Edit components

---
+++ src/app/components/user/edit/edit.component.ts
@@ -0,0 +1,55 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+import { Role } from '../../../interfaces/role';
+
+@Component({
+  selector: 'app-edit',
+  standalone: true,
+  templateUrl: './edit.component.html',
+  styleUrl: './edit.component.css',
+  imports: [
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EditComponent {
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  surnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.min(5),
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  roleControl = new FormControl<Role | null>(null, Validators.required);
+  roles: Role[] = [{ name: 'Administrador' }, { name: 'Vendedor' }];
+}
