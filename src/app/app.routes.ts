a8693d3972aac7816ac3d2cacaac0c5c0697aa1e
Arlie Rivas <arliedamir@gmail.com>
2024-08-26 23:07:40 -0500

    Change Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { UserAddComponent } from './components/user/user-add/user-add.component';
 import { UserIndexComponent } from './components/user/user-index/user-index.component';
 import { UserEditComponent } from './components/user/user-edit/user-edit.component';
+import { ClientIndexComponent } from './components/client/client-index/client-index.component';
+import { ClientAddComponent } from './components/client/client-add/client-add.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -18,5 +20,20 @@ export const routes: Routes = [
     path: 'user/add',
     component: UserAddComponent,
     title: 'Registrar Usuario',
+  },
+  {
+    path: 'user/edit',
+    component: UserEditComponent,
+    title: 'Registrar Usuario',
+  },
+  {
+    path: 'client/index',
+    component: ClientIndexComponent,
+    title: 'Indice de Clientes',
+  },
+  {
+    path: 'client/add',
+    component: ClientAddComponent,
+    title: 'Registrar Cliente',
   },
 ];
