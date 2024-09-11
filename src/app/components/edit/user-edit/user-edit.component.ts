d9e13175ae81f2a45de54c14e07623308d314de5
Arlie Rivas <arliedamir@gmail.com>
2024-09-11 18:19:36 -0500

    Add user crud functionalities

--- src/app/components/edit/user-edit/user-edit.component.ts
+++ src/app/components/edit/user-edit/user-edit.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import {
+  FormBuilder,
+  FormGroup,
   FormControl,
   Validators,
   FormsModule,
@@ -11,7 +13,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { Role } from '../../../interfaces/role';
 import { UserService } from '../../../services/user.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { User } from '../../../interfaces/user';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-user-edit',
@@ -30,53 +34,103 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserEditComponent {
-  userId: string = '';
-  user: any;
+  userId: number;
+  userForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]+$'),
+    ]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]+$'),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.min(5),
+      Validators.pattern(
+        '^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};\'":\\\\|,.<>\\/?]*$'
+      ),
+    ]),
+    state: new FormControl<boolean>(true, Validators.required),
+    role: new FormControl<number>(0, Validators.required),
+  });
 
   constructor(
+    private router: Router,
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     private userService: UserService
-  ) {}
+  ) {
+    this.userId = Number(this.route.snapshot.paramMap.get('id'));
+    this.userForm = this.fb.group({
+      name: [''],
+      surname: [''],
+      email: [''],
+      password: [''],
+      state: [true],
+      role: [0],
+    });
+  }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.userId = id;
-      this.loadUser();
+    if (this.userId) {
+      this.getUser(this.userId);
     } else {
-      // Manejar el caso donde el ID no está presente en la URL
       console.error('No ID found in the URL');
     }
   }
 
-  loadUser(): void {
-    //TODO
+  getUser(id: number) {
+    this.userService.getUser(id).subscribe({
+      next: (user: any) => {
+        this.userForm.patchValue({
+          name: user.name,
+          surname: user.name,
+          email: user.email,
+          password: user.password,
+          state: user.state,
+          role: user.role,
+        });
+      },
+      error: (error) => {
+        console.error('Error loading category:', error);
+      },
+    });
   }
 
-  nameFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern('^[a-zA-Z ]+$'),
-  ]);
-  surnameFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern('^[a-zA-Z ]+$'),
-  ]);
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  passwordFormControl = new FormControl('', [
-    Validators.required,
-    Validators.min(5),
-    Validators.pattern(
-      '^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};\'":\\\\|,.<>\\/?]*$'
-    ),
-  ]);
+  async editUser() {
+    if (this.userForm.valid) {
+      const user: User = {
+        id: this.userId,
+        name: this.userForm.value.name!,
+        surname: this.userForm.value.surname!,
+        email: this.userForm.value.email!,
+        password: this.userForm.value.password!,
+        state: this.userForm.value.state!,
+        role: {
+          id: this.userForm.value.role!,
+        },
+      };
+
+      try {
+        await firstValueFrom(this.userService.editUser(user));
+        console.log('User edited');
+        this.router.navigate(['/user/index']);
+      } catch (error) {
+        console.error('Error editing user:', error);
+      }
+    }
+  }
+
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
-  roleControl = new FormControl<Role | null>(null, Validators.required);
-  roles: Role[] = [{ name: 'Administrador' }, { name: 'Vendedor' }];
+
+  roles: Role[] = [
+    { id: 1, name: 'ADMINISTRADOR' },
+    { id: 2, name: 'VENDEDOR' },
+  ];
 }
